7210e0c0938daffc663286d1fd0cd597522a4195
ahmetkavl <56724084+ahmetkavl@users.noreply.github.com>
2021-04-27 00:00:47 +0300

    Add files via upload

---
+++ disc.sql
@@ -0,0 +1,9 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+	('stones', 'Taş', 1, 0, 1),
+	('washedstones', 'Yıkanmış Taş', 1, 0, 1),
+	('gold', 'Altın', 1, 0, 1),
+	('diamond', 'Elmas', 1, 0, 1),
+	('copper', 'Bakır', 1, 0, 1),
+	('iron', 'Demir', 1, 0, 1),
+	('coal', 'Kömür', 1, 0, 1)
+;
---
+++ esx.sql
@@ -0,0 +1,9 @@
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+	('stones', 'Taş', 10, 0, 1),
+	('washedstones', 'Yıkanmış Taş', 10, 0, 1),
+	('gold', 'Altın', 10, 0, 1),
+	('diamond', 'Elmas', 10, 0, 1),
+	('copper', 'Bakır', 10, 0, 1),
+	('iron', 'Demir', 10, 0, 1),
+	('coal', 'Kömür', 10, 0, 1)
+;
