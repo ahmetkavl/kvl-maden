311627018bf90e59b069eb43cf406f91437ae775
ahmetkavl <56724084+ahmetkavl@users.noreply.github.com>
2021-04-27 09:55:44 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -58,3 +58,15 @@ AddEventHandler("kvl-maden:mixingstones", function()
         end
     end
 end)
+
+ESX.RegisterServerCallback('kvl-maden:checkItem', function(source, cb, item, gereklisayi)
+	local xPlayer = ESX.GetPlayerFromId(source)
+    local itemcount = xPlayer.getInventoryItem(item).count
+    local itemname = xPlayer.getInventoryItem(item).label
+	if itemcount >= gereklisayi then
+		cb(true)
+	else
+        activity = 0
+        TriggerClientEvent('esx:showNotification', source, 'Üzerinde yeterli '..itemname..' yok!')
+	end
+end)
